867b914fc21f6cd892267596165b65b8b44dea67
manishreddy5353 <bmanish20849@gmail.com>
2025-01-24 12:06:54 +0530

    first commit

---
+++ PrimeNo4.java
@@ -0,0 +1,22 @@
+package jan24;
+
+public class PrimeNo4 {
+
+	public static void main(String[] args) {
+				for(int i=200,k=0;i<=500;i++) {
+					int num = i;
+					int counter=0;
+					for(int p=1;p<=num;p++) {
+						if(num % p == 0)counter++;
+				    }
+					if(counter == 2) {
+						System.out.print(num+" ");
+						k++;
+						if(k==8) {
+						System.out.println();
+						k=0;
+						}
+					}
+				}
+			}
+		}
