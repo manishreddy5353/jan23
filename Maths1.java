1369cbbac3d03798be126aa867945717c231f10e
manishreddy5353 <bmanish20849@gmail.com>
2025-01-24 12:41:04 +0530

    first commit

---
+++ Maths1.java
@@ -0,0 +1,15 @@
+package jan24;
+
+public class Maths1 {
+
+	public static void main(String[] args) {
+		for(int r=5;r>=1;r--) {
+			for(int c=5;c>=r;c--) {
+				System.out.print(c+"");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
